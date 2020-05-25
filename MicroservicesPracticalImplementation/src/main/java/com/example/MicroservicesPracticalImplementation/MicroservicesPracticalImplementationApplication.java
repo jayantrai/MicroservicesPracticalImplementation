9f586216072added45daf37753cb517a940bb2e8
Jayant Rai <jayant.chamling@outlook.com>
2020-05-25 17:53:50 -0500

    creating project with actuator and web dependency

---
+++ MicroservicesPracticalImplementation/src/main/java/com/example/MicroservicesPracticalImplementation/MicroservicesPracticalImplementationApplication.java
@@ -0,0 +1,13 @@
+package com.example.MicroservicesPracticalImplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesPracticalImplementationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesPracticalImplementationApplication.class, args);
+	}
+
+}
---
+++ MicroservicesPracticalImplementation/src/test/java/com/example/MicroservicesPracticalImplementation/MicroservicesPracticalImplementationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MicroservicesPracticalImplementation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesPracticalImplementationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
